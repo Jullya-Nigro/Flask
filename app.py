3db742f343dc1f50c0a6086703015734c9e9d0d2
Jullya-Nigro <nigrojullya07@gmail.com>
2025-10-05 13:06:47 -0300

    fix: ajustar rotas para carregar index.html e register.html

--- app.py
+++ app.py
@@ -1,62 +1,20 @@
-from flask import Flask, request, jsonify # type: ignore
+import os
+from flask import Flask
+from config import Config
+from controllers.user_controller import UserController
+from models.user_model import User, bd
 
-class CreateUser:
-    id_counter = 0
-    list_users = []
-    def __init__(self, name, email, password):
-        CreateUser.id_counter += 1   
-        self.user = {
-            "id": CreateUser.id_counter,
-            "name": name,
-            "email": email,
-            "password": password
-        }
-        CreateUser.list_users.append(self.user)
+app = Flask(__name__, template_folder=os.path.join('view', 'templates'))
+app.config.from_object(Config)
 
-app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
+bd.init_app(app)
 
-@app.route("/getusers/", methods=['GET'])
-def get_users():
-    return jsonify(CreateUser.list_users),200
+with app.app_context():
+    bd.create_all()
 
-@app.route("/createusers/", methods=['POST'])
-def create_users():
-    data = request.get_json()
-    user = CreateUser(data["name"], data["email"], data["password"])
-    return jsonify(user.user)
+app.add_url_rule('/index.html', 'index', UserController.index)
+app.add_url_rule('/register.html', 'register', UserController.register, methods=['POST', 'GET'])
 
-@app.route("/users/<int:id>", methods=['PUT'])
-def update_users(id):
-    data = request.get_json()
-    tipo = data.get("tipo")
-    mudanca = data.get("mudanca")
-
-    for i in CreateUser.list_users:
-        if i["id"] == id:
-            if tipo == "email" and mudanca != i["email"]:
-                i["email"] = mudanca
-                return jsonify(i)
-
-            elif tipo == "password" and mudanca != i["password"]:
-                i["password"] = mudanca
-                return jsonify(i)
-
-            else:
-                return jsonify({"error": "Campo inválido ou valor igual ao anterior!"}), 400
-
-    return jsonify({"error":"Usuário não encontrado!"}), 404
-
-@app.route("/users/<int:id>", methods=['DELETE'])
-def delete_users(id):
-    for i in CreateUser.list_users:
-        if i["id"] == id:
-            CreateUser.list_users.remove(i)
-            return jsonify({"msg": f"Usuário {id} removido com sucesso!"}), 200
-    
-    return jsonify({"error": "Usuário não encontrado!"}), 404
-            
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-
+    app.run(debug=True, port=5002)
